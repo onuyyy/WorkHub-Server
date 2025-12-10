690dd6bc47051245a8d111d5064163607f261727
CHO GUNHEE <106148197+dkqpeo@users.noreply.github.com>
2025-12-10 17:04:30 +0900

    Fix/security (#154)
    * fix : security 수정
    
    * fix : userController 요청 mapping 순서 변경
    
    * fix : userController 요청 mapping 순서 변경

--- src/main/java/com/workhub/global/config/SecurityConfig.java
+++ src/main/java/com/workhub/global/config/SecurityConfig.java
@@ -86,7 +86,7 @@ public class SecurityConfig {
                 "http://localhost:3000"
         ));
         configuration.setAllowedMethods(Arrays.asList(
-                "GET", "POST", "PUT", "DELETE", "PATCH"
+                "GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"
         ));
         configuration.setAllowedHeaders(List.of("*"));
         configuration.setAllowCredentials(true);
