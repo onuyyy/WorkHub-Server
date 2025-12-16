a76f0327bfb24f420cb94206e471caa24d242dea
CHO GUNHEE <106148197+dkqpeo@users.noreply.github.com>
2025-12-16 11:43:40 +0900

    fix : projectNode Status값 추가 (#188)

--- src/main/java/com/workhub/projectNode/entity/NodeStatus.java
+++ src/main/java/com/workhub/projectNode/entity/NodeStatus.java
@@ -3,7 +3,7 @@ package com.workhub.projectNode.entity;
 import com.fasterxml.jackson.annotation.JsonCreator;
 
 public enum NodeStatus {
-    NOT_STARTED, IN_PROGRESS, PENDING_REVIEW, ON_HOLD, DELETED;
+    NOT_STARTED, IN_PROGRESS, PENDING_REVIEW, ON_HOLD, DELETED, DONE;
 
     @JsonCreator
     public static NodeStatus from(String value) {
