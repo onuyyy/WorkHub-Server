d948ed4310c380cfa1d87d9c9f8998e867d5ce29
onuyyy <121489473+onuyyy@users.noreply.github.com>
2025-12-20 11:13:45 +0900

    fix: checklist comment file response dto 수정 (#225)

--- src/main/java/com/workhub/checklist/dto/comment/CheckListCommentFileResponse.java
+++ src/main/java/com/workhub/checklist/dto/comment/CheckListCommentFileResponse.java
@@ -4,12 +4,14 @@ import com.workhub.checklist.entity.comment.CheckListItemCommentFile;
 
 public record CheckListCommentFileResponse(
         Long commentFileId,
+        String fileUrl,
         String fileName,
         Integer fileOrder
 ) {
     public static CheckListCommentFileResponse from(CheckListItemCommentFile file) {
         return new CheckListCommentFileResponse(
                 file.getCommentFileId(),
+                file.getFileUrl(),
                 file.getFileName(),
                 file.getFileOrder()
         );
